835ba39a3b1574df93e87960bdc4369f7eaefba2
byungju0 <byungju8797@naver.com>
2025-11-23 08:34:00 +0900

    Revert "asdf"
    This reverts commit 65272fe8d0d771921974096b641d19b394d4a8ba.

--- main.py
+++ main.py
@@ -451,4 +451,4 @@ async def websocket_endpoint(client_ws: WebSocket):
         await client_ws.close()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="127.0.0.1", port=8002)
